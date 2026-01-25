4e7fb128b9d6f934eb5989579b86c38b44d0d5a1
Louis Lam <louislam@users.noreply.github.com>
2026-01-25 18:14:02 +0800

    Run fmt

--- frontend/src/pages/TabConfig.vue
+++ frontend/src/pages/TabConfig.vue
@@ -180,29 +180,29 @@ export default defineComponent({
 
         async uploadTab() {
             this.isUploading = true;
-             try {
-                 if (this.tabFiles.length === 0) {
-                     throw new Error("Please select a file to upload");
-                 }
+            try {
+                if (this.tabFiles.length === 0) {
+                    throw new Error("Please select a file to upload");
+                }
 
-                 const file = this.tabFiles[0].file;
+                const file = this.tabFiles[0].file;
 
-                 // Try to parse the file with AlphaTab to ensure it's valid
-                 const data = await file.arrayBuffer();
+                // Try to parse the file with AlphaTab to ensure it's valid
+                const data = await file.arrayBuffer();
 
-                 const score = alphaTab.importer.ScoreLoader.loadScoreFromBytes(
-                     new Uint8Array(data),
-                     new alphaTab.Settings(),
-                 );
+                const score = alphaTab.importer.ScoreLoader.loadScoreFromBytes(
+                    new Uint8Array(data),
+                    new alphaTab.Settings(),
+                );
 
-                 const formData = new FormData();
-                 formData.append("file", file);
+                const formData = new FormData();
+                formData.append("file", file);
 
-                 const response = await fetch(baseURL + `/api/tab/${this.tabID}/replace`, {
-                     method: "POST",
-                     credentials: "include",
-                     body: formData,
-                 });
+                const response = await fetch(baseURL + `/api/tab/${this.tabID}/replace`, {
+                    method: "POST",
+                    credentials: "include",
+                    body: formData,
+                });
 
                 await checkFetch(response);
                 notify({
@@ -217,42 +217,42 @@ export default defineComponent({
                 });
             } finally {
                 this.isUploading = false;
-             }
-         },
+            }
+        },
 
-         async uploadAudio() {
+        async uploadAudio() {
             this.isUploading = true;
-             try {
-                 if (this.audioFiles.length === 0) {
-                     throw new Error("Please select a file to upload");
-                 }
+            try {
+                if (this.audioFiles.length === 0) {
+                    throw new Error("Please select a file to upload");
+                }
 
-                 const file = this.audioFiles[0].file;
-                 const formData = new FormData();
-                 formData.append("file", file);
+                const file = this.audioFiles[0].file;
+                const formData = new FormData();
+                formData.append("file", file);
 
-                 const response = await fetch(baseURL + `/api/tab/${this.tabID}/audio`, {
-                     method: "POST",
-                     credentials: "include",
-                     body: formData,
-                 });
+                const response = await fetch(baseURL + `/api/tab/${this.tabID}/audio`, {
+                    method: "POST",
+                    credentials: "include",
+                    body: formData,
+                });
 
-                 await checkFetch(response);
-                 notify({
-                     text: "Upload audio successfully",
-                     type: "success",
-                 });
-                 this.$refs.audioDropzone.clearFiles();
-                 await this.load();
-             } catch (error) {
-                 notify({
-                     text: error.message,
-                     type: "error",
-                 });
-             } finally {
+                await checkFetch(response);
+                notify({
+                    text: "Upload audio successfully",
+                    type: "success",
+                });
+                this.$refs.audioDropzone.clearFiles();
+                await this.load();
+            } catch (error) {
+                notify({
+                    text: error.message,
+                    type: "error",
+                });
+            } finally {
                 this.isUploading = false;
-             }
-         },
+            }
+        },
 
         async saveAudio(audio) {
             let res;
@@ -484,11 +484,11 @@ export default defineComponent({
                 >
                     {{ isUploading ? "Uploading..." : "Upload" }}
                 </button>
-             </div>
-         </div>
+            </div>
+        </div>
 
-         <!-- Tab File Page -->
-         <div v-else-if='this.page === "tab-file"' class="mb-5">
+        <!-- Tab File Page -->
+        <div v-else-if='this.page === "tab-file"' class="mb-5">
             <h2 class="mt-4 mb-4">Method 1: Direct Edit</h2>
             <p>
                 If you can access the file system, you can edit/replace the tab directly, the path is:<br />
@@ -515,9 +515,9 @@ export default defineComponent({
             >
                 {{ isUploading ? "Uploading..." : "Upload" }}
             </button>
-         </div>
-     </div>
- </template>
+        </div>
+    </div>
+</template>
 
 <style scoped lang="scss">
 .menu {
